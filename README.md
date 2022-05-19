4855e2f53f97f03428bac0b03090ab77242f247c
=Jai Prakash <=>
2022-05-19 17:25:18 +0530

    Video-6-react-page-routing

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 2. Install Tools
 3. Create react App
 4. Create Git Repository
+5. Product List
+6. Add page routing
+    1. npm i react-router-dom
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
-
+import React from "react";
+import data from "./data";
 function App() {
   return (
     <div>
       <header>
-        Amazona
+        <a href="/">Amazona</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <h1>Feature Products</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+           <div className="product-info">
+           <a href={`product/${product.slug}`}>
+           <p>{product.name}</p>
+           </a>
+            <p><strong>${product.price}</strong></p>
+            <button>Add To Cart</button>
+           </div>
+          </div>
+        ))}
+        </div>
+      </main>
     </div>
   );
 }
