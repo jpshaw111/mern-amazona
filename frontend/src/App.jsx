c65f549552755f0beb1599cea7b91443ce766011
=Jai Prakash <=>
2022-05-20 12:18:58 +0530

    Video-React-js-node-express

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,26 @@
 import React from "react";
-import data from "./data";
+
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div>
+    
+      <BrowserRouter>
+      <div>
       <header>
-        <a href="/">Amazona</a>
+        <Link to="/">Amazona</Link>
       </header>
       <main>
-        <h1>Feature Products</h1>
-        <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} />
-            </a>
-           <div className="product-info">
-           <a href={`product/${product.slug}`}>
-           <p>{product.name}</p>
-           </a>
-            <p><strong>${product.price}</strong></p>
-            <button>Add To Cart</button>
-           </div>
-          </div>
-        ))}
-        </div>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />}></Route>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
+       
       </main>
-    </div>
+      </div>
+      </BrowserRouter>
+   
   );
 }
 
